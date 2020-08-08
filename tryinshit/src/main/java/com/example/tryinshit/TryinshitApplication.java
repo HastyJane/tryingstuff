9bea3922b8f8492fb9f6e72fe8d2107c8e489c3b
Hayley Smith <61245670+HastyJane@users.noreply.github.com>
2020-08-08 00:05:38 -0500

    Add files via upload

---
+++ tryinshit/src/main/java/com/example/tryinshit/TryinshitApplication.java
@@ -0,0 +1,13 @@
+package com.example.tryinshit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryinshitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryinshitApplication.class, args);
+	}
+
+}
